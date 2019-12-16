a1e1862e951e6ac543fec554c1d7281f02a9240b
joakim.palmkvist <joakim.palmkvist@seal-software.com>
2019-12-16 09:54:07 +0100

    Day16:2 WIP

--- day16/src/org/advent/FFT.java
+++ day16/src/org/advent/FFT.java
@@ -5,39 +5,86 @@ import org.advent.util.Util;
 import java.util.Arrays;
 
 public class FFT {
+    private static final int[] BASE = new int[]{0, 1, 0, -1};
+    public static boolean DEBUG = false;
     int[] start;
-    int phase;
     int[] current;
-    int[] base;
+    long msgOffset;
 
-    public FFT(int[] start) {
+    public FFT(int[] start, long offset) {
+        // start ska vara (start) repeterat 10000 gånger eller offset + 8
         this.start = start;
-        base = new int[]{0, 1, 0, -1};
         current = start;
+        this.msgOffset = offset;
     }
 
+    public static long getMsgOffset(int[] input) {
+        String offset = "";
+        for (int i = 0; i < 7; i++) {
+            offset = offset + input[i];
+        }
+
+        return Long.parseLong(offset);
+    }
+/*
+    private int[] repeatToFindOffset() {
+        long actualPointer = 0;
+        outerloop:
+        for (int i = 0; i < 10000; i++) {
+
+            if (actualPointer > msgOffset + 8) {
+                break outerloop;
+            }
+        }
+    }
+*/
     public void phase() {
-        System.out.println("Current: " + Arrays.toString(current));
+        if (DEBUG) {
+            System.out.println("Current: " + Arrays.toString(current));
+        }
         int[] next = new int[start.length];
         for (int i = 0; i < next.length; i++) {
             next[i] = calculateAtIndex(i, current);
-            System.out.println("-----------------------");
+            if (DEBUG) {
+                System.out.println("-----------------------");
+            }
         }
-        System.out.println("Result: " + Arrays.toString(next));
+        if (DEBUG) {
+
+            System.out.println("Result: " + Arrays.toString(next));
+        }
         current = next;
+    }
+
+    public int[] getMessageFromOffset() {
+        long start = msgOffset % current.length;
+        int[] msg = new int[8];
+        for (int i = 0; i < msg.length; i++) {
+            msg[i] = current[(int) start];
+            start++;
+            if (start >= current.length) {
+                start = 0;
+            }
+        }
+        return msg;
     }
 
     private int calculateAtIndex(int index, int[] array) {
         int[] indexBase = calculateBaseForIndex(index);
-        System.out.println("-- Base: " + Arrays.toString(indexBase));
+        if (DEBUG) {
+            System.out.println("-- Base: " + Arrays.toString(indexBase));
+        }
         int val = 0;
 
-        System.out.println();
 
-        System.out.print("-- : ");
+        if (DEBUG) {
+            System.out.print("-- : ");
+        }
         for (int i = 0; i < array.length; i++) {
 
-            System.out.print(array[i] + "*" + indexBase[i] + " ");
+            if (DEBUG) {
+                System.out.print(array[i] + "*" + indexBase[i] + " ");
+            }
             val += array[i] * indexBase[i];
         }
 
@@ -45,19 +92,20 @@ public class FFT {
         int[] digits = Util.toDigits(val);
 
         val = digits[digits.length - 1];
-        System.out.println(" = " + val);
+        if (DEBUG) {
+            System.out.println(" = " + val);
+        }
 
         return val;
     }
 
-    // done
     private int[] calculateBaseForIndex(int index) {
-        int[] tmp = new int[(index + 1) * base.length];
+        int[] tmp = new int[(index + 1) * BASE.length];
         int bIndex = 0;
         int omgPointer = 0;
-        for (int i = 0; i < base.length; i++) {
+        for (int i = 0; i < BASE.length; i++) {
             for (int j = 0; j < index + 1; j++) {
-                tmp[omgPointer] = base[bIndex];
+                tmp[omgPointer] = BASE[bIndex];
                 omgPointer++;
             }
             bIndex++;
--- day16/test/org/advent/Verify.java
+++ day16/test/org/advent/Verify.java
@@ -13,7 +13,7 @@ public class Verify {
     @Test
     public void test1() {
         int input = 12345678;
-        FFT fft = new FFT(Util.toDigits(input, 8));
+        FFT fft = new FFT(Util.toDigits(input, 8), 0);
         fft.phase();
         int[] result = fft.getCurrent();
 
@@ -43,7 +43,7 @@ public class Verify {
     @Test
     public void test2() {
         String input = "80871224585914546619083218645595";
-        FFT fft = new FFT(Util.toDigits(input));
+        FFT fft = new FFT(Util.toDigits(input), 0);
         for (int i = 0; i < 100; i++) {
 
             fft.phase();
@@ -58,5 +58,41 @@ public class Verify {
         Assert.assertEquals(1, result[5]);
         Assert.assertEquals(7, result[6]);
         Assert.assertEquals(6, result[7]);
+    }
+
+    @Test
+    public void step2_1() {
+        // TODO här är jag
+        // ska denna loopas 10000 gånger????
+        String input = "03036732577212944063491565474664";
+        int[] origInput = Util.toDigits(input);
+        FFT fft = new FFT(origInput, 0);
+        for (int i = 0; i < 10000; i++) {
+            fft.phase();
+        }
+        int[] realInput = fft.getCurrent();
+
+        long offset =  FFT.getMsgOffset(origInput);
+        System.out.println("Offset: " + offset);
+
+        FFT real = new FFT(realInput, offset);
+        // phase 100
+        for (int i = 0; i < 100; i++) {
+            real.phase();
+        }
+        int[] result = fft.getCurrent();
+        int[] msg = fft.getMessageFromOffset();
+
+        System.out.println("result: " + Arrays.toString(result));
+        System.out.println("msg: " + Arrays.toString(msg));
+
+        Assert.assertEquals(8, msg[0]);
+        Assert.assertEquals(4, msg[1]);
+        Assert.assertEquals(4, msg[2]);
+        Assert.assertEquals(6, msg[3]);
+        Assert.assertEquals(2, msg[4]);
+        Assert.assertEquals(0, msg[5]);
+        Assert.assertEquals(2, msg[6]);
+        Assert.assertEquals(6, msg[7]);
     }
 }
